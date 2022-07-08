55574487999f90a19e1f76cc8fc63a5806326690
Alex Betts <38247452+lina2497@users.noreply.github.com>
2022-07-08 21:26:21 +0100

    Update install_R _and_dependencies.sh

--- install_R _and_dependencies.sh
+++ install_R _and_dependencies.sh
@@ -1,15 +1,14 @@
-
+sudo apt-get install build-essential
 # Install R
-sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
-sudo apt install r-base
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo gpg --dearmor -o /usr/share/keyrings/r-project.gpg
+echo "deb [signed-by=/usr/share/keyrings/r-project.gpg] https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" | sudo tee -a /etc/apt/sources.list.d/r-project.list
+sudo apt update
+sudo apt install --no-install-recommends r-base
 
 # Install rstudio server
 sudo apt-get install gdebi-core
-wget https://download2.rstudio.org/server/xenial/amd64/rstudio-server-1.4.1103-amd64.deb
-sudo gdebi rstudio-server-1.4.1103-amd64.deb
-
+wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2022.07.0-548-amd64.deb
+sudo gdebi rstudio-server-2022.07.0-548-amd64.deb
 
 # Install tidyverse dependencies
 sudo apt install libssl-dev libcurl4-openssl-dev
@@ -111,6 +110,7 @@ Rscript -e 'pacman::p_load(
 )'
 
 # Install shiny server
-wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.16.958-amd64.deb
-sudo gdebi shiny-server-1.5.16.958-amd64.deb
+sudo apt-get install gdebi-core
+wget https://download3.rstudio.org/ubuntu-18.04/x86_64/shiny-server-1.5.18.987-amd64.deb
+sudo gdebi shiny-server-1.5.18.987-amd64.deb
 

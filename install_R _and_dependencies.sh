a40e1040b2e8b9738e09f7bc1f78eec524668dc6
Alex Betts <38247452+lina2497@users.noreply.github.com>
2022-07-09 11:15:23 +0100

    Update install_R _and_dependencies.sh

--- install_R _and_dependencies.sh
+++ install_R _and_dependencies.sh
@@ -1,4 +1,12 @@
 sudo apt-get install build-essential
+
+sudo swapoff -a
+sudo dd if=/dev/zero of=/swapfile bs=500MB count=8
+sudo mkswap /swapfile
+sudo swapon /swapfile
+grep SwapTotal /proc/meminfo
+
+
 # Install R
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo gpg --dearmor -o /usr/share/keyrings/r-project.gpg
 echo "deb [signed-by=/usr/share/keyrings/r-project.gpg] https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" | sudo tee -a /etc/apt/sources.list.d/r-project.list
